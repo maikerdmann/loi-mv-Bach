1477972e3efa6f6cd600ccda5bad8ccc49245fa8
eschaetz <88539587+eschaetz@users.noreply.github.com>
2021-08-09 12:42:09 +0200

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,58 @@
-basic.forever(function () {
-	
-})
+
+
+namespace LOI_MV {
+    /**
+     * Test-Funktion
+     */
+    //% blockId=loimvUltraschall
+    //% block="ultraschall"
+    export function ultraschall(): number {
+        return sonar.ping(DigitalPin.P8, DigitalPin.P9, PingUnit.Centimeters)
+    }
+    /**
+     * Steuert die Antriebsmotoren mit den Parametern "Power" und "Lenkung".
+     * Power gibt die Kraft von -10 bis 10 an, wobei negative Werte den Rückwärtsgang bedeuten
+     * Lenkung lässt die Motoren auf auf beiden Seiten unterschiedlich schnell bewegen, um eine Drehung des Roboters zu erzeugen. 
+     * -10 ist links, 0 gerade aus und 10 rechts
+     */
+    //% blockId=loimvAntrieb
+    //% block="antrieb"
+    export function antrieb(power: number, lenkung: number): void {
+        let speedL //Geschwindigkeit der linken Motoren
+        let speedR //Geschwindigkeit der rechten Motoren 
+        const motorMin = 200
+        if (lenkung < 0) {
+            speedR = power
+            speedL = power + 2 * power / 10 * lenkung
+        } else if (lenkung > 0) {
+            speedL = power
+            speedR = power + 2 * power / 10 * lenkung
+        } else {
+            speedL = speedR = power
+        }
+
+        if (speedL > 0) {
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            pins.digitalWritePin(DigitalPin.P13, 1)
+            pins.analogWritePin(AnalogPin.P1, Math.map(speedL, 0, 10, motorMin, 1023))
+        } else if (speedL < 0) {
+            pins.digitalWritePin(DigitalPin.P13, 0)
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            pins.analogWritePin(AnalogPin.P1, Math.map(speedL, 0, -10, motorMin, 1023))
+        } else {
+            pins.analogWritePin(AnalogPin.P1, 0)
+        }
+
+        if (speedR > 0) {
+            pins.digitalWritePin(DigitalPin.P14, 0)
+            pins.digitalWritePin(DigitalPin.P15, 1)
+            pins.analogWritePin(AnalogPin.P2, Math.map(speedR, 0, 10, motorMin, 1023))
+        } else if (speedR < 0) {
+            pins.digitalWritePin(DigitalPin.P15, 0)
+            pins.digitalWritePin(DigitalPin.P14, 1)
+            pins.analogWritePin(AnalogPin.P2, Math.map(speedR, 0, -10, motorMin, 1023))
+        } else {
+            pins.analogWritePin(AnalogPin.P2, 0)
+        }
+    }
+}
